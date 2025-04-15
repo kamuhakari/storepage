c5ece0b68ed2e463e0bb8d5ca4db3679b3363bc8
SafianB <safinkarunia93@gmail.com>
2025-04-15 09:43:59 +0700

    initial commit

---
+++ kamuhikari.js
@@ -0,0 +1,17 @@
+// toggle kelas aktif
+
+const navbarNav = document.querySelector('.navbar-nav');
+
+document.querySelector('#hamburger-menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+};
+
+// clik diluar slidebar = close navbar
+
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function(e){
+    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)){
+        navbarNav.classList.remove('active');
+    }
+});
